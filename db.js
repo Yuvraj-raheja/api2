d5db8fbec1c56d919482309349ad72ac5d09bc34
Yuvraj-raheja <yuvrajraheja25@gmail.com>
2025-11-10 16:55:42 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,17 @@
+const { MongoClient } = require('mongodb');
+const url = 'mongodb+srv://rahejayuvraj24_db_user:fDtSH06WLoyfozyx@cluster1.et9ptoc.mongodb.net'
+let db;
+const connectDB = async () => {
+  if (db) return db;
+  try {
+    const client = new MongoClient(url);
+    await client.connect();
+    db = client.db("Yuvrajdb"); // your database name
+    console.log(" Connected to MongoDB Atlas");
+    return db;
+  } catch (err) {
+    console.error(" MongoDB connection failed:", err);
+    throw err;
+  }
+};
+module.exports = connectDB;
