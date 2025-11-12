7173501d3bb896967340aa0e04e77c16cfc54427
Yuvraj-raheja <yuvrajraheja25@gmail.com>
2025-11-12 15:38:33 +0530

    Add files via upload

--- get.js
+++ get.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors');
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Import routes
 const indexRouter = require('./routes/indexxx');
+
+// Use routes
 app.use('/', indexRouter);
+
+// Start server
 app.listen(port, () => {
-    console.log("Server is running on port 3000");
+  console.log(`✅ Server is running on port ${port}`);
 });
